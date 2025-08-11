b54d144e63ba2dc88825cdb7174d816da75f307a
Priyanshu Nigam <priyanshunigam@Priyanshus-MacBook-Air-2.local>
2025-08-11 18:13:50 +0530

    update the urls

--- Backend/src/lib/utils.js
+++ Backend/src/lib/utils.js
@@ -11,8 +11,8 @@ const generateToken = (userId, role, res) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: false, // ⬅️ only set true in production
-    sameSite: "Lax",
+    secure: true, // ⬅️ only set true in production
+    sameSite: "None",
     maxAge: 3 * 24 * 60 * 60 * 1000,
   });
 
